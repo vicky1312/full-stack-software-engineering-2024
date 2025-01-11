9b9d6f3bb05f75cdb2d316d09e151bdc3d5274d4
vicky1312 <victoryabayomi@gmail.com>
2025-01-11 08:40:44 +0100

    react assignment #2 solution

---
+++ code/assignment/react/assignment2/src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css'
+import Todo from './Todo'
+import Tasks from './Task'
+import { useState } from 'react'
+function App() {
+ 
+  const [toDo, setTodo] = useState ([
+    { id: 1, task: "Serve the dog", completed: true },
+    { id: 2, task: "wash the dishes", completed: false },
+    { id: 3, task: "study", completed: false },
+    { id: 4, task: "buy groceries", completed: false },
+    { id: 5, task: "visit the library", completed: false },
+    { id: 6, task: "make dinner", completed: false },
+])
+
+const todoTask = (id) => {
+  setTodo((updateStatus) => updateStatus.map((e) => 
+      e.id === id ? {...e, completed: !e.completed }: e
+  ))
+}
+const deleteTask = (id) => {
+  setTodo((delToDo) => delToDo.filter((e) => e.id !== id))
+}
+  return (
+      <>
+      <h2>TODOS</h2>
+      <div className='input-wrapper'>
+      <Tasks
+        toDo = {toDo}
+        setTodo = {setTodo}
+     />
+      </div>
+    <div className='todo-wrapper'> {toDo.map(e => (
+      <Todo 
+      key = {e.id}
+      task = {e.task}
+      setTask = {e.task}
+      completed = {e.completed}
+      onToggle = {() => todoTask(e.id)}
+      onDelete = {() => deleteTask(e.id)}
+      />
+      
+     ))}
+     </div>
+     </>
+  )
+}
+
+export default App
